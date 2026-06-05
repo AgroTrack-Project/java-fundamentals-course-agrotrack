0ce425b360d415d7545993d79a191201918bc1b7
Eduardo <edudavid2005@hotmail.com>
2026-06-05 03:30:57 -0500

    feat: add starter file for lesson 09 arrays

---
+++ starter-files/leccion-09-arrays/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // 1. Declara un array de 4 números enteros con los valores: 5, 6, 7, 8
+        
+
+        // 2. Recorre el array con un for e imprime cada elemento
+        
+
+        // 3. Declara un array de 3 nombres (String) con los valores que quieras
+        
+
+        // 4. Imprime el primer y último elemento del array de nombres
+        
+    }
+}
